edfa72182cb256c026e61d8f9f3afb99782e7712
AnthL <anthony.langlet@gmail.com>
2013-12-29 16:20:00 +0100

    Update RocketType.java

--- src/RocketType.java
+++ src/RocketType.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
- *
- * @author azathoth
- */
+* Enum RocketType which represent different type of rocket
+* 
+*/ 
 public enum RocketType {
     ROCKET, ImpBomb, ExpBomb;
 }
