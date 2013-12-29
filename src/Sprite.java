f3f0e59d99be01f5df2c872625504e0dc7b739ac
AnthL <anthony.langlet@gmail.com>
2013-12-29 16:11:41 +0100

    Update Sprite.java

--- src/Sprite.java
+++ src/Sprite.java
@@ -1,12 +1,16 @@
-
 import java.awt.Graphics2D;
 
 /**
- *
- * @author azathoth
- */
+* Sprite is a interface of all the objects wich can be drawn
+* 
+*/
 public interface Sprite {
 
+    /**
+    *  Display the Sprite on Graphics2D
+    * 
+    * @param graphics Graphics2D where the sprite will be draw.
+    */
     public void display(Graphics2D graphics);
 
     default public void display(Graphics2D graphics, MainShuttle mainShuttle) {
