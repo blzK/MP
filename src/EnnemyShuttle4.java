3b9cf2139cecd2dd373b850306864dafebeddbad
azathoth <azathoth@NomadSoul>
2013-12-30 17:59:44 +0100

    Ennemy5 implementation

--- src/EnnemyShuttle4.java
+++ src/EnnemyShuttle4.java
@@ -1,10 +1,15 @@
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.RadialGradientPaint;
 import java.awt.Shape;
 import java.awt.geom.AffineTransform;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.GeneralPath;
+import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import org.jbox2d.collision.shapes.PolygonShape;
+import org.jbox2d.common.Timer;
 import org.jbox2d.common.Vec2;
 import org.jbox2d.dynamics.BodyDef;
 import org.jbox2d.dynamics.BodyType;
@@ -22,134 +27,132 @@ import org.jbox2d.dynamics.World;
  */
 public class EnnemyShuttle4 extends SpaceShuttle {
 
-    SpaceShuttle[] ennemyGroup = new SpaceShuttle[16];
+    private final Timer timerShapeShitfting;
 
     public EnnemyShuttle4(float x, float y, World world) {
         super(x, y);
-        float posX = x;//MasterPilot.toXCoordinates(x);
-        float posY = y;//MasterPilot.toYCoordinates(y);
 
-        for (int i = 0; i < ennemyGroup.length; i++) {
-            ennemyGroup[0] = new EnnemysubShuttle3(posX - 30, posY - 90, world);
-            ennemyGroup[1] = new EnnemysubShuttle3(posX - 30, posY - 60, world);
-            ennemyGroup[2] = new EnnemysubShuttle3(posX - 30, posY - 30, world);
-            ennemyGroup[3] = new EnnemysubShuttle3(posX - 30, posY, world);
-            ennemyGroup[4] = new EnnemysubShuttle3(posX - 30, posY + 30, world);
-            ennemyGroup[5] = new EnnemysubShuttle3(posX - 30, posY + 60, world);
-            ennemyGroup[6] = new EnnemysubShuttle3(posX - 30, posY + 90, world);
+        BodyDef bodydef2 = new BodyDef();
+        bodydef2.angle = 0;
+        bodydef2.bullet = true;
+        FixtureDef fd = new FixtureDef();
+        PolygonShape s = new org.jbox2d.collision.shapes.PolygonShape();
+        s.setAsBox(100, 20);
+        fd.filter.categoryBits = CollisionCategory.ENNEMY.getBits();
+        fd.filter.maskBits = CollisionCategory.PLAYER.getBits() | CollisionCategory.WORLD.getBits();
+        fd.shape = s;
+        fd.density = 0.0001f;
+        fd.restitution = 1f;
+        fd.friction = 1f;
 
-            ennemyGroup[7] = new EnnemysubShuttle3(posX, posY - 60, world);
-            ennemyGroup[8] = new EnnemysubShuttle3(posX, posY - 30, world);
-            ennemyGroup[9] = new EnnemysubShuttle3(posX, posY, world);
-            ennemyGroup[10] = new EnnemysubShuttle3(posX, posY + 30, world);
-            ennemyGroup[11] = new EnnemysubShuttle3(posX, posY + 60, world);
-            ennemyGroup[12] = new EnnemysubShuttle3(posX + 30, posY - 30, world);
-            ennemyGroup[13] = new EnnemysubShuttle3(posX + 30, posY, world);
-            ennemyGroup[14] = new EnnemysubShuttle3(posX + 30, posY + 30, world);
-            ennemyGroup[15] = new EnnemysubShuttle3(posX + 60, posY, world);
+        bodydef2.position.set(x, y);
+        bodydef2.type = BodyType.DYNAMIC;
+        setBody(world.createBody(bodydef2));
+        getBody().setUserData("ennemy2");
+        getBody().createFixture(fd);
+        getBody().setAngularDamping(3);
+        timerShapeShitfting = new Timer();
+        timerShapeShitfting.reset();
 
-        }
-    }
-
-    @Override
-    public void display(Graphics2D graphics) {
-        boolean allDead = true;
-        for (int i = 0; i < ennemyGroup.length; i++) {
-            allDead = allDead && ennemyGroup[i].isDead();
-        }
-        if (allDead == true) {
-            die();
-        }
-        for (int i = 0; i < ennemyGroup.length; i++) {
-            ennemyGroup[i].display(graphics);
-        }
     }
 
     @Override
     public boolean die() {
-
-        return super.die();
+        return super.die(); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public void applyLinearImpulse(Vec2 impulse, Vec2 point, Graphics2D graphics) {
-        for (int i = 0; i < ennemyGroup.length; i++) {
-            ennemyGroup[i].applyLinearImpulse(impulse, point, graphics); //To change body of generated methods, choose Tools | Templates.
+    public void display(Graphics2D graphics) {
+        super.display(graphics);
+        if (getBody().getContactList() != null) {
+            if (isActive()) {
+                die();
+            }
         }
+        if (isDead() == false) {
+            if (isActive()) {
+                graphics.setColor(Color.RED);
+                float posX = MasterPilot.toXCoordinates(getBody().getPosition().x);
+                float posY = MasterPilot.toYCoordinates(getBody().getPosition().y);
+                float x1Points[] = {
+                    posX - 10, posX + 10, posX + 30, posX + 30, posX + 10, posX - 10, posX - 30, posX - 30
+                };
+                float y1Points[] = {
+                    posY - 30, posY - 30, posY - 10, posY + 10, posY + 30, posY + 30, posY + 10, posY - 10
+                };
+                GeneralPath polygon = new GeneralPath(GeneralPath.WIND_EVEN_ODD, x1Points.length);
+                polygon.moveTo(x1Points[0], y1Points[0]);
 
+                for (int index = 1; index < x1Points.length; index++) {
+                    polygon.lineTo(x1Points[index], y1Points[index]);
+                }
+
+                polygon.closePath();
+                graphics.setPaint(Color.RED);
+                AffineTransform transform = new AffineTransform();
+                transform.rotate(getBody().getAngle(),
+                        MasterPilot.toXCoordinates(getBody().getPosition().x), MasterPilot.toYCoordinates(getBody().getPosition().y)
+                );
+
+                Shape transformed = transform.createTransformedShape((Shape) polygon);
+                graphics.fill(transformed);
+            } else {
+                Point2D center = new Point2D.Float(x, y);
+                RadialGradientPaint paint = new RadialGradientPaint(center, 1000,
+                        new float[]{
+                            0.4f,
+                            1f},
+                        new Color[]{
+                            Color.black,
+                            new Color(37, 197, 246)
+                        });
+                graphics.setPaint(paint);
+
+                Shape shape = new Ellipse2D.Float(MasterPilot.toXCoordinates(getBody().getPosition().x), MasterPilot.toYCoordinates(getBody().getPosition().y), 70f, 70f);
+                graphics.fill(shape);
+            }
+        }
     }
 
-    @Override
-    void applyAngularImpulse(float f) {
-        for (int i = 0; i < ennemyGroup.length; i++) {
-            ennemyGroup[i].applyAngularImpulse(f);
-        }
+    private boolean isActive()
 
-    }
-
-    @Override
-    void applyForce(Vec2 vec2, Vec2 position) {
-        for (int i = 0; i < ennemyGroup.length; i++) {
-            ennemyGroup[i].applyForce(vec2, position);
-        }
-    }
-
-    @Override
-    public void fire(Graphics2D graphics, RocketType rocketType, float x, float y, Vec2 direction, CollisionCategory col) {
-        for (int i = 0; i < ennemyGroup.length; i++) {
-            ennemyGroup[i].fire(graphics, rocketType, x, y, direction, col); //To change body of generated methods, choose Tools | Templates.
-        }
+    {
+return timerShapeShitfting.getMilliseconds() % 20000 < 10000;
     }
 
     @Override
     public void behave(MainShuttle mainShuttle, Graphics2D graphics) {
+        Vec2 vecDiff = mainShuttle.getPosition().sub(this.getPosition());
+
         if (isDead() == false) {
-            Vec2 vecDiff = mainShuttle.getPosition().sub(ennemyGroup[0].getPosition());
-
-            fire(graphics, RocketType.ROCKET, x, y, mainShuttle.getPosition(), CollisionCategory.WORLD);
-
-            for (int i = 0; i < ennemyGroup.length; i++) {
-                ennemyGroup[i].behave(mainShuttle, graphics);
-                if (i >= 1) {
-                    Vec2 vecDiffToMainshuttle = mainShuttle.getPosition().sub(ennemyGroup[i].getPosition());
-                    float r = (float) Math.random() * 0.001f;
-                    if (vecDiffToMainshuttle.length() > 700) {
-                        ennemyGroup[i].applyForce(vecDiffToMainshuttle.mul(r).negate(), ennemyGroup[i].getPosition());
-                        ennemyGroup[i].applyForce(vecDiffToMainshuttle.mul(r).skew(), ennemyGroup[i].getPosition());
-                    } else if (vecDiffToMainshuttle.length() < 300) {
-                        ennemyGroup[i].applyForce(vecDiffToMainshuttle.mul(r), ennemyGroup[i].getPosition());
-                        ennemyGroup[i].applyForce(vecDiffToMainshuttle.mul(r).skew().negate(), ennemyGroup[i].getPosition());
+            if (isActive()) {
+                if (getTimer().getMilliseconds() > 200) {
+                    float posX = MasterPilot.toYCoordinates(getBody().getPosition().x) + 100;
+                    float posY = MasterPilot.toYCoordinates(getBody().getPosition().y) ;
+                    fire(graphics, RocketType.ROCKET, posX, posY, vecDiff, CollisionCategory.ENNEMY);
+                    getTimer().reset();
+                } else {
+                    double angle = this.getAngle() % (2 * Math.PI) + Math.PI;
+                    double angleVec = Math.atan2(vecDiff.x, vecDiff.y);//+Math.PI*2;
+                    if (angle > angleVec) {
+                        this.applyAngularImpulse(-10f);
+                    } else {
+                        this.applyAngularImpulse(10f);
                     }
+                    if (vecDiff.length() > 400) {
+                        this.applyForce(vecDiff.mul(0.2f), this.getPosition());
+                    } else if (vecDiff.length() < 300) {
+                        this.applyForce(vecDiff.mul(0.2f).negate(), this.getPosition());
 
+                    }
+                    
+                this.applyForce(vecDiff.skew().mul((float)(Math.random()*2-1)).mul(0.1f), this.getPosition());
                 }
-            }
+            } else {
 
+                this.applyForce(getBody().getLinearVelocity().negate(), this.getPosition());
+            }
         }
-//        if (isDead() == false) {
-//            if (getTimer().getMilliseconds() > 200) {
-//                float posX = MasterPilot.toYCoordinates(getBody().getPosition().x) + 100;
-//                float posY = MasterPilot.toXCoordinates(getBody().getPosition().y) - 100;
-//                fire(graphics, RocketType.ROCKET, posX, posY, vecDiff, CollisionCategory.ENNEMY);
-//
-//                getTimer().reset();
-//            } else {
-////        double angle = (this.getAngle() +Math.PI)%(Math.PI*2);
-//                double angle = this.getAngle() % (2 * Math.PI) + Math.PI;
-//
-//                double angleVec = Math.atan2(vecDiff.x, vecDiff.y);//+Math.PI*2;
-////                System.out.println("angle " + Math.toDegrees(angle) + " angle  " + Math.toDegrees(angleVec));
-//                if (angle > angleVec) {
-//                    this.applyAngularImpulse(0.02f);
-//                } else {
-//                    this.applyAngularImpulse(-0.02f);
-//                }
-//                if (vecDiff.length() > 200) {
-//                    this.applyForce(vecDiff, this.getPosition());
-//                }
-//                this.applyForce(vecDiff.skew().negate(), this.getPosition());
-//            }
-//        }
 
     }
-
 }
